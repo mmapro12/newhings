8950b728a7e3e5a5068a4d07a798ea72d4f45178
mmapro12 <asia172007@gmail.com>
2023-11-15 10:48:21 +0300

    Adding decorator to main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-print("This is Ali Version")
+from time import time
 
-name = input(">>Enter your name: ")
-print(f"Hello {name.strip()} !")
+def speedTest(func):
+    def wrapper():
+        start = time()
+        func()
+        end = time()
+
+        print(f"Function time: {end-start}")
+    
+    return wrapper
+
+
+@speedTest
+def func():
+    print("Hello every one como estas?")
+
+func()
